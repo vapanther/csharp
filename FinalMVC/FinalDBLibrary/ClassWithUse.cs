d80885924953c001f48aa9a0de361b30175df703
Ankit Kumar Tripathi <Ankit.Tripathi@rsystems.com>
2018-11-21 18:11:15 +0530

    FINAL MVC for IIS

---
+++ FinalMVC/FinalDBLibrary/ClassWithUse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalDBLibrary
+{
+    public class ClassWithUse
+    {
+        public static void takeReference()
+        {
+            Cross18Entities abc = new Cross18Entities();
+            abc.Employees.ToList();
+        }
+        public static void useRefernce()
+        {
+            ClassWithUse.takeReference();
+        }
+    }
+}
---
+++ FinalMVC/FinalMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using FinalDBLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        Cross18Entities dbcontext = new Cross18Entities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(dbcontext.Employees.ToList());
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                dbcontext.Employees.Add(employee);
+                dbcontext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+    }
+}
